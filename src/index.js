82e82d32865a4dd4a11bc8dd9041566c761a148d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-20 22:05:25 +0500

    utils custom api resoponse and error handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/dbConnection.js";
+import { app } from "./app.js";
 
 dotenv.config({path: "./env"})
 
-connectDB(); 
+connectDB()
+.then( () => {
+    try{
+        app.on("error", (error) => {
+            console.log(`ERROR: ${error}`);
+            throw error;
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running at PORT ${process.env.PORT}`);
+        })
+    } catch(err){
+        console.log(`Error: ${err}`);
+        
+    }
+})
+.catch(( err ) => {
+    console.log(`MongoDB connection failed: ${err}`);
+    
+}); 
