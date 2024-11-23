ef639845bbd3718f8d2b9eefed6be9035dde6b49
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-23 14:59:43 +0500

    video model created

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import {model, Schema} from "mongoose";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, //duration will also come from cloudinary
+            required: true
+        },
+        view: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Video = model("Video", videoSchema);
+
+export {Video};
