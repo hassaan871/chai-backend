7e24c4ec1f22eda1cb1fc8251302ca2bc781c60b
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-23 17:42:40 +0500

    added mongoose pre hook to encrypt password before save using bcrypt third party package

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import {model, Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -49,6 +51,14 @@ const userSchema = new Schema(
         timestamps: true
     }
 );
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = model("User", userSchema);
 
