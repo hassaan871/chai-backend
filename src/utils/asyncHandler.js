0d7666bc482a96218e779786a1be9fd7ada62c81
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-26 01:19:21 +0500

    all setup configured, error while importing user.controller in user.routes.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,8 +10,8 @@
 // }
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler()).reject((err) => next(err))
+    return (req, res, next) => {
+        Promise.resolve(requestHandler()).catch((err) => next(err))
     }
 }
  
