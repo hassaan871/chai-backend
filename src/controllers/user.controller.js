5b125cdb9b2eff04e279b1007e4e27f46843d16d
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-28 16:17:04 +0500

    coverImage required bug resolved but facing new bug avatar image required while i am sending tha avatar in req

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,7 +25,14 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    // fixed the bug "cover image not sent"
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
 
     if (!avatarLocalPath) { throw new ApiError(400, "Avatar Image is required. ") }
 
