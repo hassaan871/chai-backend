3131d412f219aa1fc376b4e135a47ed73b81a834
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-28 15:24:05 +0500

    ulinked avatar and cover image from the server after successfully upload on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,13 +14,14 @@ const uploadOnCloudinary = async (localFilePath) => {
         // upload the file on cloudinary 
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
-        })
+        });
         // file has been uploaded successfully
-        console.log(`file is uploaded on cloudinary ${response.url}`);
+        // console.log(`file is uploaded on cloudinary ${response.url}`);
+        fs.unlink(localFilePath);
         return response;
         
     } catch (error) {
-        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
