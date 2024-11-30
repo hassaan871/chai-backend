069922df6ed98239a753a03ba5bb4e1f8c39d9a6
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-30 22:04:05 +0500

    Add validation to ensure either username or email is provided in API request

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -79,7 +79,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     const { email, username, password } = req.body;
 
-    if (!email || !username) { throw new ApiError(400, "username or email is required") }
+    if (!(email || username)) { throw new ApiError(400, "username or email is required") }
 
     const user = await User.findOne([{ email }, { username }]);
 
